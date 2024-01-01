b7c6a564f51f9330ffcee48b1ee739f379dd5b2f
jshuk-7 <Jessieshukeireh2001@gmail.com>
2023-12-31 23:29:42 -0500

    fixed collisions + added ptr key type

--- ordered_map/src/map.h
+++ ordered_map/src/map.h
@@ -6,7 +6,7 @@
 #include <stdbool.h>
 #include <assert.h>
 
-#define VHASH_MAP_SIZE 10
+#define VHASH_MAP_CAPACITY 10
 #define VHASH_MAP_ASSERT(expr, msg) assert((expr))
 
 typedef enum {
@@ -14,45 +14,60 @@ typedef enum {
 	KEY_TYPE_F32,
 	KEY_TYPE_CHAR,
 	KEY_TYPE_STR,
+	KEY_TYPE_PTR,
 } KeyType;
 
 typedef struct {
-	void* data[VHASH_MAP_SIZE];
+	void* data[VHASH_MAP_CAPACITY];
+	size_t size;
 } HashMap;
 
 HashMap vhash_map_create(void);
+size_t vhash_map_size(const HashMap* this);
 bool vhash_map_insert(HashMap* this, KeyType type, const void* key, void* value);
 bool vhash_map_insert_i(HashMap* this, const int key, void* value);
 bool vhash_map_insert_f(HashMap* this, const float key, void* value);
 bool vhash_map_insert_c(HashMap* this, const char key, void* value);
 bool vhash_map_insert_str(HashMap* this, const char* key, void* value);
+bool vhash_map_insert_ptr(HashMap* this, const void* key, const void* value);
 void* vhash_map_erase(HashMap* this, KeyType type, const void* key);
 void* vhash_map_erase_i(HashMap* this, const int key);
 void* vhash_map_erase_f(HashMap* this, const float key);
 void* vhash_map_erase_c(HashMap* this, const char key);
 void* vhash_map_erase_str(HashMap* this, const char* key);
+void* vhash_map_erase_ptr(HashMap* this, const void* key);
 void* vhash_map_find(const HashMap* this, KeyType type, const void* key);
 void* vhash_map_find_i(const HashMap* this, const int key);
 void* vhash_map_find_f(const HashMap* this, const float key);
 void* vhash_map_find_c(const HashMap* this, const char key);
 void* vhash_map_find_str(const HashMap* this, const char* key);
+void* vhash_map_find_ptr(const HashMap* this, const void* key);
 void vhash_map_debug_print(const HashMap* this);
 
-uint64_t vhash_i(int value);
-uint64_t vhash_f(float value);
-uint64_t vhash_c(char value);
+uint64_t vhash_i(const int value);
+uint64_t vhash_f(const float value);
+uint64_t vhash_c(const char value);
 uint64_t vhash_str(const char* value);
+uint64_t vhash_ptr(const void* value);
 
 #endif
 
 #ifdef VHASH_MAP_IMPLEMENTATION
 
 HashMap vhash_map_create(void) {
-	HashMap map = { .data = NULL };
-	for (int i = 0; i < VHASH_MAP_SIZE; i++) {
+	HashMap map = { .data = NULL, .size = 0 };
+	for (int i = 0; i < VHASH_MAP_CAPACITY; i++) {
 		(&map)->data[i] = NULL;
 	}
 	return map;
+}
+
+size_t vhash_map_size(const HashMap* this) {
+	if (this == NULL) {
+		return 0;
+	}
+
+	return this->size;
 }
 
 bool vhash_map_insert(HashMap* this, KeyType type, const void* key, void* value) {
@@ -77,14 +92,19 @@ bool vhash_map_insert_i(HashMap* this, const int key, void* value) {
 	}
 
 	const uint64_t hash = vhash_i(key);
-	if (this->data[hash] != NULL) {
-		printf("HashMap collision occurred with key: '%d'\n", key);
-		return false;
+
+	for (int i = 0; i < VHASH_MAP_CAPACITY; i++) {
+		const int attempt = (i + hash) % VHASH_MAP_CAPACITY;
+		if (this->data[attempt] == NULL) {
+			// free space
+			this->data[attempt] = value;
+			this->size++;
+			return true;
+		}
 	}
-	else {
-		this->data[hash] = value;
-		return true;
-	}
+
+	printf("failed to insert key: '%d', collision occurred\n", key);
+	return false;
 }
 
 bool vhash_map_insert_f(HashMap* this, const float key, void* value) {
@@ -93,14 +113,19 @@ bool vhash_map_insert_f(HashMap* this, const float key, void* value) {
 	}
 
 	const uint64_t hash = vhash_f(key);
-	if (this->data[hash] != NULL) {
-		printf("HashMap collision occurred with key: '%f'\n", key);
-		return false;
+
+	for (int i = 0; i < VHASH_MAP_CAPACITY; i++) {
+		const int attempt = (i + hash) % VHASH_MAP_CAPACITY;
+		if (this->data[attempt] == NULL) {
+			// free space
+			this->data[attempt] = value;
+			this->size++;
+			return true;
+		}
 	}
-	else {
-		this->data[hash] = value;
-		return true;
-	}
+
+	printf("failed to insert key: '%f', collision occurred\n", key);
+	return false;
 }
 
 bool vhash_map_insert_c(HashMap* this, const char key, void* value) {
@@ -109,14 +134,19 @@ bool vhash_map_insert_c(HashMap* this, const char key, void* value) {
 	}
 
 	const uint64_t hash = vhash_c(key);
-	if (this->data[hash] != NULL) {
-		printf("HashMap collision occurred with key: '%c'\n", key);
-		return false;
+
+	for (int i = 0; i < VHASH_MAP_CAPACITY; i++) {
+		const int attempt = (i + hash) % VHASH_MAP_CAPACITY;
+		if (this->data[attempt] == NULL) {
+			// free space
+			this->data[attempt] = value;
+			this->size++;
+			return true;
+		}
 	}
-	else {
-		this->data[hash] = value;
-		return true;
-	}
+
+	printf("failed to insert key: '%c', collision occurred\n", key);
+	return false;
 }
 
 bool vhash_map_insert_str(HashMap* this, const char* key, void* value) {
@@ -125,13 +155,40 @@ bool vhash_map_insert_str(HashMap* this, const char* key, void* value) {
 	}
 
 	const uint64_t hash = vhash_str(key);
-	if (this->data[hash] != NULL) {
-		printf("HashMap collision occurred with key: '%s'\n", key);
-		return false;
-	} else {
-		this->data[hash] = value;
-		return true;
+
+	for (int i = 0; i < VHASH_MAP_CAPACITY; i++) {
+		const int attempt = (i + hash) % VHASH_MAP_CAPACITY;
+		if (this->data[attempt] == NULL) {
+			// free space
+			this->data[attempt] = value;
+			this->size++;
+			return true;
+		}
 	}
+
+	printf("failed to insert key: '%s', collision occurred\n", key);
+	return false;
+}
+
+bool vhash_map_insert_ptr(HashMap* this, const void* key, void* value) {
+	if (this == NULL || value == NULL) {
+		return false;
+	}
+
+	const uint64_t hash = vhash_ptr(key);
+
+	for (int i = 0; i < VHASH_MAP_CAPACITY; i++) {
+		const int attempt = (i + hash) % VHASH_MAP_CAPACITY;
+		if (this->data[attempt] == NULL) {
+			// free space
+			this->data[attempt] = value;
+			this->size++;
+			return true;
+		}
+	}
+
+	printf("failed to insert key: '%p', collision occurred\n", key);
+	return false;
 }
 
 void* vhash_map_erase(HashMap* this, KeyType type, const void* key) {
@@ -144,6 +201,7 @@ void* vhash_map_erase(HashMap* this, KeyType type, const void* key) {
 		case KEY_TYPE_F32: return vhash_map_erase_f(this, (*(const float*)key));
 		case KEY_TYPE_CHAR: return vhash_map_erase_c(this, (*(const char*)key));
 		case KEY_TYPE_STR: return vhash_map_erase_str(this, (const char*)key);
+		case KEY_TYPE_PTR: return vhash_map_erase_ptr(this, (void*)key);
 	}
 
 	VHASH_MAP_ASSERT(false, "unknown key type!");
@@ -159,6 +217,7 @@ void* vhash_map_erase_i(HashMap* this, const int key) {
 	if (this->data[hash] != NULL) {
 		void* data = this->data[hash];
 		this->data[hash] = NULL;
+		this->size--;
 		return data;
 	}
 
@@ -174,6 +233,7 @@ void* vhash_map_erase_f(HashMap* this, const float key) {
 	if (this->data[hash] != NULL) {
 		void* data = this->data[hash];
 		this->data[hash] = NULL;
+		this->size--;
 		return data;
 	}
 
@@ -189,6 +249,7 @@ void* vhash_map_erase_c(HashMap* this, const char key) {
 	if (this->data[hash] != NULL) {
 		void* data = this->data[hash];
 		this->data[hash] = NULL;
+		this->size--;
 		return data;
 	}
 
@@ -204,6 +265,23 @@ void* vhash_map_erase_str(HashMap* this, const char* key) {
 	if (this->data[hash] != NULL) {
 		void* data = this->data[hash];
 		this->data[hash] = NULL;
+		this->size--;
+		return data;
+	}
+
+	return NULL;
+}
+
+void* vhash_map_erase_ptr(HashMap* this, const void* key) {
+	if (this == NULL) {
+		return NULL;
+	}
+
+	const uint64_t hash = vhash_ptr(key);
+	if (this->data[hash] != NULL) {
+		void* data = this->data[hash];
+		this->data[hash] = NULL;
+		this->size--;
 		return data;
 	}
 
@@ -278,25 +356,38 @@ void* vhash_map_find_str(const HashMap* this, const char* key) {
 	return NULL;
 }
 
+void* vhash_map_find_ptr(const HashMap* this, const void* key) {
+	if (this == NULL) {
+		return NULL;
+	}
+
+	const uint64_t hash = vhash_ptr(key);
+	if (this->data[hash] != NULL) {
+		return this->data[hash];
+	}
+
+	return NULL;
+}
+
 void vhash_map_debug_print(const HashMap* this) {
 	if (this == NULL) {
 		return;
 	}
 
-	for (int i = 0; i < VHASH_MAP_SIZE; i++) {
+	for (int i = 0; i < VHASH_MAP_CAPACITY; i++) {
 		printf("\t%i.\t%u\n", i, this->data[i] == NULL ? 0 : 1);
 	}
 }
 
-uint64_t vhash_i(int value) {
+uint64_t vhash_i(const int value) {
 	return 5;
 }
 
-uint64_t vhash_f(float value) {
+uint64_t vhash_f(const float value) {
 	return 5;
 }
 
-uint64_t vhash_c(char value) {
+uint64_t vhash_c(const char value) {
 	return 5;
 }
 
@@ -308,10 +399,14 @@ uint64_t vhash_str(const char* value) {
 		for (int j = 0; j < rounds; j++) {
 			char c = value[i];
 			hash = hash + c;
-			hash = (hash * c) % VHASH_MAP_SIZE;
+			hash = (hash * c) % VHASH_MAP_CAPACITY;
 		}
 	}
 	return hash;
+}
+
+uint64_t vhash_ptr(const void* value) {
+	return 5;
 }
 
 #endif
--- ordered_map/src/test_app.c
+++ ordered_map/src/test_app.c
@@ -8,10 +8,18 @@ int main(int argc, char** argv) {
 	int a = 10;
 	int b = 11;
 	int c = 12;
+	int d = 13;
+	int e = 14;
+	int f = 15;
+	int g = 16;
 	const KeyType type = KEY_TYPE_STR;
 	vhash_map_insert(&map, type, "Jack", &a);
 	vhash_map_insert(&map, type, "Fred", &b);
 	vhash_map_insert(&map, type, "Amy", &c);
+	vhash_map_insert(&map, type, "Eliza", &d);
+	vhash_map_insert(&map, type, "Ron", &e);
+	vhash_map_insert(&map, type, "Kelly", &f);
+	vhash_map_insert(&map, type, "Will", &g);
 
 	vhash_map_erase(&map, type, "Amy");
 
@@ -22,6 +30,9 @@ int main(int argc, char** argv) {
 	} else {
 		printf("'%s' -> '%d'\n", key, *(int*)result);
 	}
+
+	const size_t size = vhash_map_size(&map);
+	printf("Size: %zu\n", size);
 
 	vhash_map_debug_print(&map);
 	return getchar();
